6a7d2654c433e405019cfe25f33a0ae820f10bea
TraMyNguyen <116280668+TraMyNguyen39@users.noreply.github.com>
2024-04-17 17:39:34 +0700

    add uid into supervisor (#7)
    * update phone icon to email icon
    
    * add uid to supervisor

--- app/src/main/java/com/example/falldetection/data/model/User.kt
+++ app/src/main/java/com/example/falldetection/data/model/User.kt
@@ -3,10 +3,11 @@ package com.example.falldetection.data.model
 import java.util.Date
 
 data class User(
+    var uid: String,
     val address: String,
     val birthDate: Date,
     val email: String,
     val fullName: String
 ) {
-    constructor() : this("", Date(), "", "")
+    constructor() : this("", "", Date(), "", "")
 }
